4de70fec551c628d5f28ac1445e9f64300e1adf1
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-19 15:45:11 +0530

    Updates

---
+++ precache-manifest.c3961e45e6d73aa479b7f77af396af7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c3fb04d5d3607523fa1e33b5478389",
+    "url": "/LameJokes/index.html"
+  },
+  {
+    "revision": "026646c4c981bb229424",
+    "url": "/LameJokes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6d394b0e5d0d917e044",
+    "url": "/LameJokes/static/js/2.8762d80e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LameJokes/static/js/2.8762d80e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026646c4c981bb229424",
+    "url": "/LameJokes/static/js/main.51980233.chunk.js"
+  },
+  {
+    "revision": "3042e1b512f91a56387f",
+    "url": "/LameJokes/static/js/runtime-main.9877c803.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LameJokes/static/media/logo.5d5d9eef.svg"
+  }
+]);
